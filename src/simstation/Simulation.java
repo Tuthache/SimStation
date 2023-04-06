2d2411e29b32c13d001d0ee0c5778a13efd30c29
KyleJamesSJSU <kyle.james@sjsu.edu>
2023-04-06 12:04:18 -0700

    added professor's sample code for Simulation

--- src/simstation/Simulation.java
+++ src/simstation/Simulation.java
@@ -1,7 +1,31 @@
 package simstation;
 
+import java.util.*;
 import mvc.*;
 
 public class Simulation extends Model {
     // TODO add variables and methods to Simulation
+
+    // added professor's code below
+    private Timer timer;
+    private int clock;
+
+    private void startTimer() {
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new ClockUpdater(), 1000, 1000);
+    }
+
+    private void stopTimer() {
+        timer.cancel();
+        timer.purge();
+    }
+
+    private class ClockUpdater extends TimerTask {
+        public void run() {
+            clock++;
+            //changed();
+        }
+    }
+
+    // etc.
 }
